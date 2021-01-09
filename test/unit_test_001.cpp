13d27ff449cf9058a47899bc2290d800149463e3
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-10 00:07:55 +0100

    Fix test (#4)
    * fix unit test

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -23,13 +23,10 @@
 
 #include <ArduinoUnitTests.h>
 
-#define assertEqualFloat(arg1, arg2, arg3)  assertOp("assertEqualFloat", "expected", fabs(arg1 - arg2), compareLessOrEqual, "<=", "actual", arg3)
-#define assertEqualINF(arg)  assertOp("assertEqualINF", "expected", INFINITY, compareEqual, "==", "actual", arg)
-#define assertEqualNAN(arg)  assertOp("assertEqualNAN", "expected", true, compareEqual, "==", "actual", isnan(arg))
-
 
 #include "Arduino.h"
 #include "HX711.h"
+
 
 uint8_t dataPin = 6;
 uint8_t clockPin = 7;
@@ -43,19 +40,6 @@ unittest_teardown()
 {
 }
 
-/*
-unittest(test_new_operator)
-{
-  assertEqualINF(exp(800));
-  assertEqualINF(0.0/0.0);
-  assertEqualINF(42);
-  
-  assertEqualNAN(INFINITY - INFINITY);
-  assertEqualNAN(0.0/0.0);
-  assertEqualNAN(42);
-}
-*/
-
 
 unittest(test_constructor)
 {
@@ -66,7 +50,6 @@ unittest(test_constructor)
 
   assertTrue(scale.is_ready());  // pins are default LOW apparently.
 
-  // assertEqual(1, 1);
 }
 
 unittest(test_gain)
@@ -132,7 +115,6 @@ unittest(test_tare)
 
   scale.set_offset(123);
   assertTrue(scale.tare_set());
-
 }
 
 
@@ -151,8 +133,6 @@ unittest(test_unit_price)
   scale.set_offset();
   assertEqual(0, scale.get_offset());
 }
-
-
 
 
 unittest_main()
